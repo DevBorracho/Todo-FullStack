b0ea01330d3d6f2b6879996f45c81245e54a55c4
Jhonathan Feliz <sakurakojisakura008@gmail.com>
2025-10-03 12:25:38 -0400

    completo

--- src/index.js
+++ src/index.js
@@ -18,12 +18,6 @@ app.use("/api", tasksRoutes);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const router = Router();
-
-router.get("/tasks", getTasksController);
-router.post("/tasks", postTaskController);
-router.delete("/tasks/:id", deleteTaskController);
-
 app.use(express.static(path.join(__dirname, "../client")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/index.html"));
