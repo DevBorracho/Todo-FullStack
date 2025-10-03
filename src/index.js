89931840e2cc4864af81e32c571d4ed1ec9f12fc
Jhonathan Feliz <sakurakojisakura008@gmail.com>
2025-10-03 12:27:15 -0400

    completo

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "../client")));
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/index.html"));
 });
 
