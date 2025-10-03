64846927896f20f0c02d2a804c2ed72b355976e7
Jhonathan Feliz <sakurakojisakura008@gmail.com>
2025-10-03 12:30:50 -0400

    completo

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "../client")));
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/index.html"));
+  res.sendFile(path.join(__dirname, "../client/HTML/index.html"));
 });
 
 // Iniciar servidor
